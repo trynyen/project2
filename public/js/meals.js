807f5543cf1c79f74c1096a8dd5b4df340dd7cd9
ayoussefinia <alex.youssefinia@gmail.com>
2019-07-18 11:13:53 -0500

    message

--- public/js/meals.js
+++ public/js/meals.js
@@ -1,9 +1,20 @@
 console.log("meals js was called");
 
 $(".meal-option").on("click", function(){
-  var quantity = $(this).data("id");
-  console.log(quantity);
+  var mealId = $(this).data("id");
+  console.log(mealId);
   // $.put('api/meals', quantity, function(meal) {
   //   console.log(meal)
   // })
+  $.ajax({
+    url: '/api/meals',
+    type: 'PUT',
+    data: {mealId},
+    success: function(response) {
+      console.log(response);
+      $.post('/api/orders', {mealId}, function(response){
+        console.log(response);
+      })
+    }
+ });
 }); 
