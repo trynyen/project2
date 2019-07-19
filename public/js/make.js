a4d765b8e12bd920ec9314b634a42608f056afc6
Megan Wu <meganwu1994@gmail.com>
2019-07-19 13:52:19 -0500

    validation functions modified again

--- public/js/make.js
+++ public/js/make.js
@@ -1,11 +1,10 @@
 console.log("make js was called");
+var mealName;
+var quantity;
+var zip;
+var phone1; 
 
 function postMeal() {
-  var mealName = $("#meal-name").val();
-  var quantity =$("#quantity").val();
-  var zip = String($("#zip").val());
-  var phone1 = String($("#phone1").val()); 
-
   var postObj = {
     "name": mealName,
     "quantity": Number(quantity),
@@ -18,35 +17,40 @@ function postMeal() {
     console.log(meal);
     window.location.href = "\meals";
   });
-}
+};
 
 $("#make").on("click", function(){
-  zipCodeValidate();
-  phonenumber();
-  console.log("2 functions are called");
+  mealName = $("#meal-name").val();
+  quantity =$("#quantity").val();
+  zip = $("#zip").val();
+  phone1 = $("#phone1").val(); 
+  console.log(mealName, quantity, zip, phone1);
+  if (zipCodeValidate() && phonenumber()) {
+    postMeal();
+  }
 });
 
 function zipCodeValidate() {
-  var zipcode = /^\d{5}$/;
-  if(zip.value.match(zipcode)) {
-    postMeal();
-    console.log("meal posted");
+  var zipcode = new RegExp("/^\d{5}$/");
+  // console.log("zip is a " + typeOf)
+  if(zip.match(zipcode)) {
+    return true;
     
   } else {
     alert("Please enter a valid zip code");
-    console.log("not posted");
   }
 }
 
 
 function phonenumber() {
-  var phoneno = /^\d{10}$/;
-  if(phone1.value.match(phoneno)) {
-    postMeal();
-    console.log("meal posted");
+  var phoneno = new RegExp("/^\d{10}$/");
+  if(phone1.match(phoneno)) {
+    return true;
 
   } else {
     alert("Please enter a valid zip code");
-    console.log("not posted");
   }
 }
+
+module.exports = zipCodeValidate;
+module.exports = phonenumber;
--- test/canary.test.js
+++ test/canary.test.js
@@ -1,9 +1,10 @@
 var expect = require("chai").expect;
 
-describe("canary test", function() {
+describe("phonenumber", function() {
   // A "canary" test is one we set up to always pass
   // This can help us ensure our testing suite is set up correctly before writing real tests
-  it("should pass this canary test", function() {
-    expect(true).to.be.true;
+  it("should be 10 digits", function() {
+    expect(phonenumber(0987654321)).to.equal.(10);
   });
 });
+
