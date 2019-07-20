427b6f338d30c5d37f71d2f5ddad10f786ea081c
Megan Wu <meganwu1994@gmail.com>
2019-07-20 14:42:22 -0500

    got rid of the canary test for now

--- test/canary.test.js
+++ test/canary.test.js
@@ -1,10 +1,10 @@
 var expect = require("chai").expect;
 
-describe("phonenumber", function() {
-  // A "canary" test is one we set up to always pass
-  // This can help us ensure our testing suite is set up correctly before writing real tests
-  it("should be 10 digits", function() {
-    expect(phonenumber(0987654321)).to.equal.(10);
-  });
-});
+// describe("phonenumber", function() {
+//   // A "canary" test is one we set up to always pass
+//   // This can help us ensure our testing suite is set up correctly before writing real tests
+//   it("should be 10 digits", function() {
+//     expect(phonenumber(0987654321)).to.equal.(10);
+//   });
+// });
 
