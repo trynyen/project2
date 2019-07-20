39c9dca15d2dfff0496fe264c91c99f84ddd1b50
Robbie F <el.robbie96@gmail.com>
2019-07-20 16:48:32 -0500

    made one validation function for both zipcode and phone number

--- public/js/make.js
+++ public/js/make.js
@@ -27,35 +27,52 @@ $("#make").on("click", function(){
   zip = $("#zip").val();
   phone1 = $("#phone1").val(); 
   console.log(mealName, quantity, zip, phone1);
-  if (zipCodeValidate() && phonenumber()) {
+  if (valiDate()) {
     postMeal();
   }
 });
 
 
 
-function zipCodeValidate() {
-  var zipcode = new RegExp("/^\d{5}$/");
-  // console.log("zip is a " + typeOf)
-  if(zip.length === 5) {
-    return true;
+// function zipCodeValidate() {
+//   var zipcode = new RegExp("/^\d{5}$/");
+//   // console.log("zip is a " + typeOf)
+//   if(zip.length === 5) {
+//     return true;
     
-  } else {
-    alert("Please enter a valid zip code");
-    window.location.href = "/home";
-    return false;
-  }
-}
+//   } else {
+//     alert("Please enter a valid zip code");
+//     window.location.href = "/home";
+//     return false;
+//   }
+// }
 
 
-function phonenumber() {
-  var phoneno = new RegExp("/^\d{10}$/");
-  if(phone1.length === 10) {
+// function phonenumber() {
+//   var phoneno = new RegExp("/^\d{10}$/");
+//   if(phone1.length === 10) {
+//     return true;
+
+//   } else {
+//     alert("Please enter a valid Phone Number");
+//     window.location.href = "/home";
+//     return false;
+//   }
+// }
+
+function valiDate() {
+  if(zip.length === 5 && phone1.length === 10) {
     return true;
-
-  } else {
+  } else if(zip.length != 5 && phone1.length === 10) {
+    alert("Please enter a valid Zipcode");
+    window.location.href = "/home";
+  } else if(zip.length === 5 && phone1.length != 10) {
     alert("Please enter a valid Phone Number");
     window.location.href = "/home";
+  }
+    else {
+    alert("Please enter valid Zipcode and Phone Number");
+    window.location.href = "/home";
     return false;
   }
-}
+};
